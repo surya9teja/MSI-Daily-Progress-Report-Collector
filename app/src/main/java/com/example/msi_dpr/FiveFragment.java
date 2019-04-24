21624503cfc6e775015fbebc20cdf76616c58915
surya9teja <suryateja516101@gmail.com>
2019-04-24 16:31:10 +0530

    Added separate dashboard activity

--- app/src/main/java/com/example/msi_dpr/FiveFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.msi_dpr;
-
-import android.support.v4.app.Fragment;
-
-class FiveFragment extends Fragment {
-}
--- app/src/main/java/com/example/msi_dpr/data_retrieval.java
+++ app/src/main/java/com/example/msi_dpr/data_retrieval.java
@@ -1,52 +1,46 @@
 package com.example.msi_dpr;
 
-import android.os.Build;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-public class data_retrieval extends AppCompatActivity {
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
+
+
+public class data_retrieval extends AppCompatActivity{
+
+    private float[] yData = { 78,22 };
+    private String[] xData = { "Work Done","Pending" };
+    PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_retrieval);
-        toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        viewPager=findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-        tabLayout=findViewById(R.id.tablayout);
-        tabLayout.setupWithViewPager(viewPager);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.data_retrieval_color));
-        }
-        
-    }
-
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        String project[] = new String[]{"AP- 1132 & 636km", "AP- 242km", "BSPTCL", "GETCO- 2226", "GETCO- 2274 P1", "GETCO- 2274 P2", "GETCO- 2275", "GETCO- 2276", "GETCO- 2278", "PGCIL-1851km", "PGCIL-721km", "PTCUL", "TS-216km"};
-        adapter.addFrag(new OneFragment(), project[0]);
-        adapter.addFrag(new TwoFragment(), project[1]);
-        adapter.addFrag(new ThreeFragment(), project[2]);
-        adapter.addFrag(new FourFragment(), project[3]);
-        adapter.addFrag(new FiveFragment(), project[4]);
-        adapter.addFrag(new SixFragment(), project[5]);
-        adapter.addFrag(new SevenFragment(), project[6]);
-        adapter.addFrag(new EightFragment(), project[7]);
-        adapter.addFrag(new NineFragment(), project[8]);
-        adapter.addFrag(new TenFragment(), project[9]);
-        adapter.addFrag(new ElevenFragment(), project[10]);
-        adapter.addFrag(new TwelveFragment(), project[11]);
-        adapter.addFrag(new ThirteenFragment(), project[12]);
-        viewPager.setAdapter(adapter);
+        //pieChart=findViewById(R.id.piechart);
+//        pieChart.setUsePercentValues(true);
+//        pieChart.setDrawHoleEnabled(false);
+//        pieChart.setRotationAngle(0);
+//        pieChart.setRotationEnabled(true);
+//        ArrayList<PieEntry> entries= new ArrayList<>();
+//            entries.add(new PieEntry(50,0));
+//            entries.add(new PieEntry(60,1));
+//        PieDataSet dataSet=new PieDataSet(entries,"Work Progress");
+//        ArrayList<String> xVal= new ArrayList<>();
+//            xVal.add("Work Done");
+//            xVal.add("Pending");
+//            PieData data = new PieData(dataSet);
+//        data.setValueFormatter(new PercentFormatter());
+//        pieChart.setData(data);
+//        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+//        data.setValueTextSize(13f);
+//        data.setValueTextColor(Color.DKGRAY);
+//        pieChart.animateXY(1400, 1400);
     }
 }
