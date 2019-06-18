6e35bbf2b8f5ffe4d1c38e09f4ffc11139451ee0
Surya Teja <suryateja516101@gmail.com>
2019-06-18 18:08:24 +0530

    Roles added

--- app/src/main/java/com/example/msi_dpr/MainActivity.java
+++ app/src/main/java/com/example/msi_dpr/MainActivity.java
@@ -16,23 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (user != null) {
-                    // User is signed in
-                    Intent i = new Intent(MainActivity.this,data_post.class);
-                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(i);
-                    MainActivity.this.finish();
-                } else {
-                    // User is signed out
-                    final Intent mainIntent = new Intent(MainActivity.this, login.class);
-                    startActivity(mainIntent);
-                    MainActivity.this.finish();
-                }
+                final Intent mainIntent = new Intent(MainActivity.this, login.class);
+                startActivity(mainIntent);
+                MainActivity.this.finish();
             }
         }, 2000);
     }
--- app/src/main/java/com/example/msi_dpr/data_retrieval.java
+++ app/src/main/java/com/example/msi_dpr/data_retrieval.java
@@ -6,127 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+/*ap_1= AP1132KM&636KM
+* ap_2=AP242KM
+* bsp=BSPTCL
+* get_1=GETCO2226
+* get_2=GETCO2274 P1
+* get_3=GETCO2274 P2
+* get_4=GETCO2275
+* get_5=GETCO2278
+* pg_1=PGCIL1721
+* pg_2=PGCIL1851
+* ts=TS216
+* pt=PTCUL*/
 
 public class data_retrieval extends AppCompatActivity{
-
-    Button ap1132,ap242,bsptcl,getco2226,getco2274p1,getco2274p2,getco2275,getco2278,pgcil1721,ts216,getco2276,pgcil1851,ptcul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_retrieval);
-        ap1132= findViewById(R.id.ap1132);
-        ap242=findViewById(R.id.ap242);
-        bsptcl=findViewById(R.id.bsptcl);
-        getco2226=findViewById(R.id.getco2226);
-        getco2274p1=findViewById(R.id.getco2274p1);
-        getco2274p2=findViewById(R.id.getco2274p2);
-        getco2275=findViewById(R.id.getco2275);
-        getco2278=findViewById(R.id.getco2278);
-        pgcil1721=findViewById(R.id.pgcil1721);
-        ts216=findViewById(R.id.ts216);
-        getco2276=findViewById(R.id.getco2276);
-        pgcil1851=findViewById(R.id.pgcil1851);
-        ptcul=findViewById(R.id.ptcul);
-        ap1132.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(data_retrieval.this,ap1132.class);
-                startActivity(intent);
-            }
-        });
-        ap242.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(data_retrieval.this,ap242.class);
-                startActivity(intent);
-            }
-        });
-        bsptcl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent (data_retrieval.this,bsptcl.class);
-            startActivity(intent);
-            }
-        });
-        getco2276.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2226.class);
-                startActivity(intent);
-            }
-        });
-        getco2274p1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2274p1.class);
-                startActivity(intent);
-            }
-        });
-
-        getco2274p2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2274p2.class);
-                startActivity(intent);
-            }
-        });
-        getco2275.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2225.class);
-                startActivity(intent);
-            }
-        });
-        getco2278.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2228.class);
-                startActivity(intent);
-            }
-        });
-        pgcil1721.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,pgcil1721.class);
-                startActivity(intent);
-            }
-        });
-        ts216.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,ts216.class);
-                startActivity(intent);
-            }
-        });
-        getco2276.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,getco2276.class);
-                startActivity(intent);
-            }
-        });
-        pgcil1851.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,pgcil1851.class);
-                startActivity(intent);
-            }
-        });
-        ptcul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,ptcul.class);
-                startActivity(intent);
-            }
-        });
-        ap1132.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(data_retrieval.this,ap1132.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
